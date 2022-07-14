9caa0054e3a765641a0d25a1d0292d1e84d56832
Jie Qiu <stulalala@163.com>
2022-07-14 17:22:35 +0800

    framework

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <div>botname: {{ bot_name }}</div>
+    <div>botrating: {{ bot_rating }} </div>
   </div>
   <router-view/>
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+
+export default {
+  name: "APP",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    $.ajax({
+      url: "http://localhost:3333/pk/getbotinfo/",
+      type: "get",
+      success: resp => {
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      }
+    })
+
+    return {
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-image: url("~@/assets/background.png");
 }
 </style>
